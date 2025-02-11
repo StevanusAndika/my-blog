edde70fc10eed76d0734fb5570290ad127a57c12
Stevanus Andika <stevanus0602@gmail.com>
2025-02-12 02:23:43 +0700

    cance

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,7 +1,6 @@
 <x-layout>
     <x-slot:title>All Posts</x-slot:title>
 
-<<<<<<< HEAD
     <div class="py-8 max-w-screen-xl mx-auto">
         <button id="openModalBtn" 
             class="mb-6 px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700">
@@ -78,22 +77,3 @@
         });
     </script>
 </x-layout>
-=======
-  @foreach ($posts  as $post)
-  
-
-
-  <article class="py-8 max-w-screen-md border-b border-gray-300">
-    <a href="{{ $post['id'] }}" class="hover:underline">
-    <h2 class="mb-1 text-3xl tracking-tighter font-bold text-gray-900">{{ $post ['title'] }}</h2>
-  </a>
-    <div class="text-base text-gray-500">
-      <a href="#">{{ $post['author'] }} |01 Februari 2025</a>
-    </div>
-    <p class="my-4 font-light">{{ Str::limit($post['body'],100) }}</p>
- <a href="{{ $post['id'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
-  </article>
-  @endforeach
-  
-</x-layout>
->>>>>>> parent of 8a6f529 (add view single post and add  support arr library in routes)
