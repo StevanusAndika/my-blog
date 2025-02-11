8a56b4dee8d629839b9a8895309b232b59aae71c
Stevanus Andika <stevanus0602@gmail.com>
2025-02-11 12:40:45 +0700

    latihan basic laravel

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Halaman Page</title>
+</head>
+<body>
+<a href="/">Home</a>
+<a href="/">about</a>
+<a href="/">blog</a>
+<a href="/blog">contact</a>
+<h1>{{ $isi }}</h3>
+<h2>blog 1 </h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium commodi amet eaque recusandae placeat, consequuntur omnis? Quasi error ullam id accusamus cupiditate molestiae aliquid, soluta, eligendi mollitia similique at saepe!</p>
+<h3>blog 2 </h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium commodi amet eaque recusandae placeat, consequuntur omnis? Quasi error ullam id accusamus cupiditate molestiae aliquid, soluta, eligendi mollitia similique at saepe!</p>
+    
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,22 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+
 });
+
+Route::get('about', function () {
+    return view('about',['nama' => 'Stevanus Andika']);
+    
+});
+
+Route::get('blog', function () {
+    return view('blog',['isi' => 'My blog']);
+    
+});
+Route::get('social', function () {
+    return view('social_media',['contact' => 'My contact']);
+    
+});
+
+
