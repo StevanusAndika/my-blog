8a6f529f3d1111930959079a27365e97520e62eb
Stevanus Andika <stevanus0602@gmail.com>
2025-02-11 18:58:12 +0700

    add view single post and add  support arr library in routes

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,20 +1,19 @@
 <x-layout>
   <x-slot:title>{{ $title }}</x-slot:title>
 
-  @foreach ($posts  as $post)
-  
-
-
-  <article class="py-8 max-w-screen-md border-b border-gray-300">
-    <a href="{{ $post['id'] }}" class="hover:underline">
-    <h2 class="mb-1 text-3xl tracking-tighter font-bold text-gray-900">{{ $post ['title'] }}</h2>
-  </a>
+  @foreach ($posts as $post)
+<article class="py-8 max-w-screen-md border-b border-gray-300">
+    <a href="{{ url('posts/' . $post['id']) }}" class="hover:underline">
+        <h2 class="mb-1 text-3xl tracking-tighter font-bold text-gray-900">{{ $post['title'] }}</h2>
+    </a>
     <div class="text-base text-gray-500">
-      <a href="#">{{ $post['author'] }} |01 Februari 2025</a>
+        <a href="#">{{ $post['author'] }} |01 Februari 2025</a>
     </div>
-    <p class="my-4 font-light">{{ Str::limit($post['body'],100) }}</p>
- <a href="{{ $post['id'] }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
-  </article>
-  @endforeach
+    <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
+    <a href="{{ url('posts/' . $post['id']) }}" class="font-medium text-blue-500 hover:underline">Read More &raquo;</a>
+</article>
+@endforeach
+
+
   
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,14 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('home',['title'=>'Home Page']);
-
+    return view('home', ['title' => 'Home Page']);
 });
 
 Route::get('about', function () {
-    return view('about',['title' => 'About Page']);
-    
+    return view('about', ['title' => 'About Page']);
 });
 
 Route::get('posts', function () {
@@ -17,13 +16,13 @@ Route::get('posts', function () {
         'title' => 'Blog',
         'posts' => [
             [
-                'id'=>1,
+                'id' => 1,
                 'title' => 'Judul Artikel 1',
                 'author' => 'Steven',
                 'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim provident omnis possimus cumque eum fuga nisi mollitia corrupti ab nesciunt!'
             ],
             [
-                'id' =>2,
+                'id' => 2,
                 'title' => 'Judul Artikel 2',
                 'author' => 'Stevan',
                 'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim provident omnis possimus cumque eum fuga nisi mollitia corrupti ab nesciunt!'
@@ -31,10 +30,38 @@ Route::get('posts', function () {
         ]
     ]);
 });
+Route::get('/posts/{id}', function ($id) {
+    $posts = [
+        [
+            'id'=> 1,
+            'title' => 'Judul Artikel 1',
+            'author' => 'Steven',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim provident omnis possimus cumque eum fuga nisi mollitia corrupti ab nesciunt!'
+        ],
+        [
+            'id' => 2,
+            'title' => 'Judul Artikel 2',
+            'author' => 'Stevan',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim provident omnis possimus cumque eum fuga nisi mollitia corrupti ab nesciunt!'
+        ],
+    ];
 
-Route::get('contact', function () {
-    return view('social_media',['title' => 'My contact']);
-    
+    // Menggunakan Arr::first untuk mencari post berdasarkan ID
+    $post = Arr::first($posts, function ($post) use ($id) {
+        return $post['id'] == $id;
+    });
+
+    // Jika tidak ditemukan, tampilkan 404
+    if (!$post) {
+        abort(404);
+    }
+
+    return view('post', [
+        'title' =>'single post'.$post['title'],
+        'post' => $post
+    ]);
 });
 
-
+Route::get('contact', function () {
+    return view('social_media', ['title' => 'My contact']);
+});
